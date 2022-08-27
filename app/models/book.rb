9ced11bf467e0961680e09655691e5e91eda98d7
Carlos Alan <cardosocarlosalan@gmail.com>
2022-08-27 12:57:15 -0300

    add pg search in book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,11 @@ class Book < ApplicationRecord
   validates :name, presence: true
   validates :author, presence: true
   validates :storage_place, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_author,
+    against: [ :name, :author ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end

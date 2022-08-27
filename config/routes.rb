a81b80d512d69920aae7b9d65ba61e293745ac5b
Carlos Alan <cardosocarlosalan@gmail.com>
2022-08-27 11:52:53 -0300

    create books routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :books, only: :index
 end
